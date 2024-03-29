f8cc35c1ce9d1135dbc07feee5baa0c59e6b55d9
sr-sagar <sagarramani992@gmail.com>
2024-03-29 14:45:50 +0530

    added a transaction page with color system

--- client/src/components/Transcations.jsx
+++ client/src/components/Transcations.jsx
@@ -8,31 +8,56 @@ const Transcations_Page = () => {
 
     
     const [temp,setTemp] = useState([]);
+    const [color,setColor] = useState('');
     useEffect(() => {
-        for(let i = 0; i < 10; i++){
-            setTemp((val) => [...val,i])
-        }
+        fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(json => setTemp(json))
         
     },[])
     console.log(temp);
+
+    const trans_checker = (Deposit) => {
+        if(Deposit != ""){
+            setColor('#3FFF00');
+        }
+        else{
+            setColor('red');
+        }
+    }
+
+    useEffect(() => {
+        temp.forEach(trans => {
+            trans_checker(trans.phone)
+        })
+
+    },[temp])
+
     return(
         <>
             <div className="trans-navbar">
                 <NavBar />
             </div>
             <div className="trans-main-section">
-                {/* space for balance component  */}
-                {
-                    temp.forEach((trans) => {
-                            console.log(trans);
-                            <ul>
-                                <span key={trans}>
-                                    <li>{trans}</li>
-                                </span>
-                            </ul>
-                    })
+                <ul className="trans-titles">
+                    <li>Date</li>
+                    <li>Transaction Details</li>
+                    <li>Withdrawals</li>
+                    <li>Deposits</li>
+                    <li>Balance</li>
+                </ul>
+                <ul>
+                    {temp.map(trans => {
+                    return(
+                            <span key={trans.id}>
+                                <li>{trans.name}</li>
+                                <li>{trans.email}</li>
+                                <li style={{color: color}}>{trans.id}</li>
+                                <li style={{color: color}}>{trans.phone}</li>
+                                <li>{trans.username}</li>
+                            </span>
 
-                }
+)
+                    })}
+                </ul>
             </div>
         </>
     )
