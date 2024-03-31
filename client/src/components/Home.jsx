c45fe90354be7576bf5dd496b3c892775e78809f
sr-sagar <sagarramani992@gmail.com>
2024-03-31 11:02:47 +0530

    updated home page and linked login system

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -19,12 +19,18 @@ const Home = () => {
             </div>
             <div className="main-section">
                 <h2 className="home-title">Open Source Payment App</h2>
-                <button className="transaction-btn" onClick={() => {navigate('/transactions')}}><GrTransaction size={30}/></button>
-                <p>Transcations</p>
-                <button className="send-money-btn"><IoSend size={30}/></button>
-                <p>Send Money</p>
-                <button className="account-btn"><MdAccountCircle size={30} /></button>
-                <p>Account Detail</p>
+                <div className="home-trans-section">
+                    <button className="transaction-btn" onClick={() => {navigate('/transactions')}}><GrTransaction size={30}/></button>
+                    <p>Transcations</p>
+                </div>
+                <div className="home-send-money-section">
+                    <button className="send-money-btn"><IoSend size={30}/></button>
+                    <p>Send Money</p>
+                </div>
+                <div className="home-acc-details-section">
+                    <button className="account-btn"><MdAccountCircle size={30} /></button>
+                    <p>Account Detail</p>
+                </div>
 
             </div>
 
