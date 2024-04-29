8d0e37d5925f809c37b02eba1b6eaf78996bb384
sr-sagar <sagarramani992@gmail.com>
2024-04-29 21:52:26 +0530

    trial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         <Route path={'/transactions'} element={<Transactions_Page bal={balance}/>}/>
         <Route path={'/user_login'} element={<User_Login/>}/>
         <Route path={'/user_signup'} element={<User_Signup/>}/>
-        <Route path={'/account_details'} element={<Account_Details bal={balance} />}/>
+        <Route path={'/account_details'} element={<Account_Details />}/>
         <Route path={'/send_money'} element={<Send_Money />}/>
 
       </Routes>
