be4ebad7e23b0782db58323bdb7d350cc61699f8
sr-sagar <sagarramani992@gmail.com>
2024-04-29 20:21:30 +0530

    added validation in the navbar and home page

--- client/src/components/Custom_Aleart.jsx
+++ client/src/components/Custom_Aleart.jsx
@@ -3,15 +3,16 @@ import '../css files/custom_aleart.css';
 import { useNavigate } from 'react-router-dom';
 
 
-const CustomAleart = ({msg,pos}) => {
+const CustomAleart = ({msg,pos,setIsClicked}) => {
     const navigate = useNavigate();
+
 
     // const aleart = ({msg}) => {
         return(
             <div className="custom-aleart" style={{top: pos + "%"}}>
                 {/* <p>User Created Successfully...</p> */}
                 <p>{msg}</p>
-                <button onClick={() => {navigate('/')}}>Ok</button>
+                <button onClick={() => {navigate('/'); setIsClicked(false)}}>Ok</button>
             </div>
         )
     // }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,30 +5,71 @@ import { MdAccountCircle } from "react-icons/md";
 import { IoSend } from "react-icons/io5";
 import { GrTransaction } from "react-icons/gr";
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+import CustomAleart from "./Custom_Aleart";
+import { useEffect } from "react";
 
 
 const Home = () => {
 
     const navigate = useNavigate();
+    const [isClicked,setIsClicked] = useState(false)
+    const [isLoggedIn,setIsLoggedIn] = useState(false)
     
+    const featchUser = async() => {
+        try{
+
+            const res = await fetch("http://localhost:3000/user/getUser",{
+                method: 'GET',
+                headers: {
+                    "auth-token": localStorage.getItem('jsonwebtoken')
+                }
+            })
+
+            const data = await res.json()
+            const name = data.name
+            setIsLoggedIn(name)
+        }catch(e)
+        {
+            console.log(e)
+        }
+    }
+    useEffect(() => {featchUser()},[])
+
+    const checkLogin = (login,navigation_link) => {
+        if(login)
+        {
+            navigate(navigation_link);
+        }
+        else{
+            setIsClicked(true)
+        }
+    }
 
     return(
         <>
             <div className="home-navbar">
-                <NavBar />
+                <NavBar isClicked={isClicked} setIsClicked={setIsClicked}/>
+            </div>
+            <div>
+                {isClicked?
+                    <CustomAleart pos={15} msg={"Please Signup/Login first to use those features..."} setIsClicked={setIsClicked}/>
+                    :
+                    <></>
+                }
             </div>
             <div className="main-section">
                 <h2 className="home-title">Open Source Payment App</h2>
                 <div className="home-trans-section">
-                    <button className="transaction-btn" onClick={() => {navigate('/transactions')}}><GrTransaction size={30}/></button>
+                    <button className="transaction-btn" onClick={() => {{checkLogin(isLoggedIn,'/transactions')}}}><GrTransaction size={30}/></button>
                     <p>Transcations</p>
                 </div>
                 <div className="home-send-money-section">
-                    <button className="send-money-btn" onClick={() => {navigate('/send_money')}}><IoSend size={30}/></button>
+                    <button className="send-money-btn" onClick={() => {checkLogin(isLoggedIn,'/send_money')}}><IoSend size={30}/></button>
                     <p>Send Money</p>
                 </div>
                 <div className="home-acc-details-section">
-                    <button className="account-btn" onClick={() => {navigate('/account_details')}}><MdAccountCircle size={30} /></button>
+                    <button className="account-btn" onClick={() => {{checkLogin(isLoggedIn,'/account_details')}}}><MdAccountCircle size={30} /></button>
                     <p>Account Detail</p>
                 </div>
 
